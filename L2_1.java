e71d7f604f6897213a84058f194872b346b3004b
NatarajanThangaraj <137900621+NatarajanThangaraj@users.noreply.github.com>
2023-07-04 13:50:05 +0530

    Update L2_1.java

--- L2_1.java
+++ L2_1.java
@@ -1,6 +1,9 @@
 /*Given an array of integers, replace every element with the next greatest element (greatest element on the
 right side) in the array. Since there is no element next to the last element, replace it with -1. For example,
 if the array is {16, 17, 4, 3, 5, 2}, then it should be modified to {17, 5, 5, 5, 2, -1}. */
+
+
+
 public class L2_1{
 public static void main(String []args){
 int []Arr={16,17,4,3,5,2};
@@ -22,10 +25,7 @@ System.out.print(x+" ");
 		
 	}
 
-	
 
-//for(int x:Ans)
-//	System.out.println(x);
 
 
 
