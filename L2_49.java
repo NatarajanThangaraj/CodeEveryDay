91393ec26fdc7c39358b856c563392180192d338
NatarajanThanagaraj <natarajanthanagarajcareer@gmail.com>
2023-07-18 11:24:31 +0530

    Finished

--- L2_49.java
+++ L2_49.java
@@ -2,15 +2,18 @@
 49. Given two Strings s1 and s2, remove all the characters from s1 which is present in s2.
 Input: s1=”expErIence”, s2=”En”
 output: s1=”exprIece” */
-
+import java.util.Scanner;
 public class L2_49 {
     public static void main(String[]args){
-       String s2="Manddetory", s1="Mdtr ";
+    	Scanner scan=new Scanner(System.in);
+    	System.out.print("Enter the Input :");
+       String s2=scan.next();
+       System.out.print("Enter what we remove :");
+       String s1=scan.next();
     int len1=s1.length();
     char x,y;
     for(int i=0;i<len1;i++){
         x=s1.charAt(i);
-
         for(int j=0;j<s2.length();j++){
             y=s2.charAt(j);
             if(x==y){
