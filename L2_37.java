023a361b02ccab8938620c7e71d861c553b9e211
NatarajanThanagaraj <natarajanthanagarajcareer@gmail.com>
2023-07-18 11:05:53 +0530

    Finished

--- L2_37.java
+++ L2_37.java
@@ -1,4 +1,3 @@
-
 /*Problem 37.:
 Evaluate the expression and sort and print the output. Getting the input is the tricky part
 Input:
@@ -18,49 +17,51 @@ import java.util.Scanner;
 public class L2_37 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		System.out.print("Enter the Input :");
+		System.out.print("Enter the N of Array :");
 		int n = scan.nextInt();
 		scan.nextLine();
 		int Arr2[] = new int[n];
 		String Arr[] = new String[n];
 		System.out.println("Enter the Array Elements:");
-		for (int i = 0; i < n; i++) {
+		int i=0;
+	while(i<n) {
 			Arr[i] = scan.nextLine();
 			String Arr1[];
 			if (Arr[i].contains("*")) {
 				Arr1 = Arr[i].split("\\*");
 				Arr2[i] = Integer.parseInt(Arr1[0]);
 				for (int j = 1; j < Arr1.length; j++) {
-					//System.out.println(Arr2[i]);
-					Arr2[i] =Arr2[i]* Integer.parseInt(Arr1[j]);
+					Arr2[i] = Arr2[i] * Integer.parseInt(Arr1[j]);
 				}
+				i++;
 			} else if (Arr[i].contains("^")) {
 				Arr1 = Arr[i].split("\\^");
 				int mult = 1;
-				//System.out.println(Arrays.toString(Arr1));
 				for (int j = 1; j < Arr1.length; j++) {
 					mult *= Integer.parseInt(Arr1[j]);
 				}
-				Arr2[i]=(int)Math.pow((double)Integer.parseInt(Arr1[0]),(double) mult);
-
+				Arr2[i] = (int) Math.pow((double) Integer.parseInt(Arr1[0]), (double) mult);
+i++;
 			} else if (Arr[i].contains("+")) {
 				Arr1 = Arr[i].split("\\+");
 				for (int j = 0; j < Arr1.length; j++) {
 					Arr2[i] += Integer.parseInt(Arr1[j]);
 				}
-			}
-			 else if (Arr[i].contains("-")) {
-					Arr1 = Arr[i].split("\\-");
-					Arr2[i] = Integer.parseInt(Arr1[0]);
-					for (int j = 1; j < Arr1.length; j++) {
-						Arr2[i] -= Integer.parseInt(Arr1[j]);
-					}
-				}
-			 else
-				 Arr2[i]=Integer.parseInt(Arr[i]);
+				i++;
+			}  else
+				Arr2[i] = Integer.parseInt(Arr[i]);
 		}
 		System.out.println(Arrays.toString(Arr2));
-
+		for (int k = 0; k < Arr2.length; k++) {
+			for (int j = k + 1; j < Arr2.length; j++) {
+				if (Arr2[k] > Arr2[j]) {
+					String temp1 = Arr[k];
+					Arr[k] = Arr[j];
+					Arr[j] = temp1;
+				}
+			}
+		}
+		System.out.println(Arrays.toString(Arr));
 	}
 
 }
