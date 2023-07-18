0ec689c0916d25e833078ef8aa1b44b6d134aa0c
NatarajanThangaraj <137900621+NatarajanThangaraj@users.noreply.github.com>
2023-07-18 11:18:39 +0530

    Finished

--- L2_50.java
+++ L2_50.java
@@ -3,7 +3,6 @@ input: array[]={6, 3, 9, 10, 8, 2, 1, 15, 7};
 output: {7, 5, 10, 15, 9, 3, 2, _, 8}
 If we are solving this question using sorting, we need to use any O(nlogn) sorting algorithm. */
 import java.util.Arrays;
-
 public class L2_50{
 	public static void main(String[]args){
 		int [] Arr={6,3,9,10,8,2,1,15,7};
